e2bc14b71e20a1677f634d466707b5eba58d9560
nzaih1999 <nzaih18@gmail.com>
2022-03-04 10:34:45 +0300

    new

--- stk_initiate.php
+++ stk_initiate.php
@@ -4,45 +4,37 @@ if(isset($_POST['submit'])){
 
   date_default_timezone_set('Africa/Nairobi');
 
-  # access token
+  //access token from the portal
   $consumerKey = '6KbWxV10jvYAf84gH7TJHe3uNhQ1v6ij'; //Fill with your app Consumer Key
   $consumerSecret = 'Kx4mi71saFQBoEgz'; // Fill with your app Secret
 
-  # define the variales
-  # provide the following details, this part is found on your test credentials on the developer account
+  
+  //provide the following details, this part is found on your test credentials on the developer account
   $BusinessShortCode = '174379';
   $Passkey = 'bfb279f9aa9bdbcf158e97dd71a467cd2e0c893059b10f78e6b72ada1ed2c919';  
   
-  /*
-    This are your info, for
-    $PartyA should be the ACTUAL clients phone number or your phone number, format 2547********
-    $AccountRefference, it maybe invoice number, account number etc on production systems, but for test just put anything
-    TransactionDesc can be anything, probably a better description of or the transaction
-    $Amount this is the total invoiced amount, Any amount here will be 
-    actually deducted from a clients side/your test phone number once the PIN has been entered to authorize the transaction. 
-    for developer/test accounts, this money will be reversed automatically by midnight.
-  */
+  
   
    $PartyA = $_POST['phone']; // This is your phone number, 
   $AccountReference = '2255';
   $TransactionDesc = 'Test Payment';
   $Amount = $_POST['amount'];;
  
-  # Get the timestamp, format YYYYmmddhms -> 20181004151020
+  // Get the timestamp, format YYYYmmddhms -> 20181004151020
   $Timestamp = date('YmdHis');    
   
-  # Get the base64 encoded string -> $password. The passkey is the M-PESA Public Key
+  //Get the base64 encoded string -> $password. The passkey is the M-PESA Public Key
   $Password = base64_encode($BusinessShortCode.$Passkey.$Timestamp);
 
-  # header for access token
+  //header for access token
   $headers = ['Content-Type:application/json; charset=utf8'];
 
-    # M-PESA endpoint urls
+    //M-PESA endpoints
   $access_token_url = 'https://sandbox.safaricom.co.ke/oauth/v1/generate?grant_type=client_credentials';
   $initiate_url = 'https://sandbox.safaricom.co.ke/mpesa/stkpush/v1/processrequest';
 
-  # callback url
-  $CallBackURL = 'https://phpii.herokuapp.com/callback_url.php';  
+  //callback url
+  $CallBackURL = 'https://phpii.herokuapp.com/callback_url.php';  //your website
 
   $curl = curl_init($access_token_url);
   curl_setopt($curl, CURLOPT_HTTPHEADER, $headers);
@@ -55,10 +47,10 @@ if(isset($_POST['submit'])){
   $access_token = $result->access_token;  
   curl_close($curl);
 
-  # header for stk push
+  // header  stk push
   $stkheader = ['Content-Type:application/json','Authorization:Bearer '.$access_token];
 
-  # initiating the transaction
+  // initiating the transaction
   $curl = curl_init();
   curl_setopt($curl, CURLOPT_URL, $initiate_url);
   curl_setopt($curl, CURLOPT_HTTPHEADER, $stkheader); //setting custom header
